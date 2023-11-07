2f18160efcc0721ca9d2e0912469c27dc30aee84
Walid Kabboussa <walidkabou@gmail.com>
2023-11-07 18:12:50 +0100

    setup share_password controller and views

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -3,22 +3,32 @@ class SharesController < ApplicationController
     before_action :set_password
 
     def new
-        @users = User.exculiding(current_user)
+        @users = User.excluding(@password.users)
         @user_password = UserPassword.new
     end
 
     def create
-
+        @user_password = @password.user_passwords.new(user_password_params)
+        if @user_password.save
+            redirect_to @password
+        else
+            render :new, status: :unprocessable_entity
+        end
     end
 
     def destroy
-
+        @password.user_passwords.where(user_id: params[:id]).destroy_all 
+        redirect_to @password
     end
 
 
     private
 
+    def user_password_params
+        params.require(:user_password).permit(:user_id)
+    end
+
     def set_password
-        @password = current_user.passwords.find(params[:pasword_id])
+        @password = current_user.passwords.find(params[:password_id])
     end
 end
