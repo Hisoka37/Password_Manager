db38fbb176c89dcbe275d3c80ffbd937b157bd46
Walid Kabboussa <walidkabou@gmail.com>
2023-11-08 14:52:05 +0100

    setup lanaging passwords roles

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -14,11 +14,8 @@ class PasswordsController  < ApplicationController
     end
 
     def create
-        # @password = current_user.passwords.create(password_params)
-        # if  @password.persisted?
         @password = Password.new(password_params)
-        @password.user_passwords.new(user: current_user )
-
+        @password.user_passwords.new(user: current_user, role: :owner)
         if @password.save
             redirect_to @password
         else  
--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -1,9 +1,9 @@
 class SharesController < ApplicationController
     before_action :authenticate_user!
     before_action :set_password
+    before_action :authorize_item, only: [:update, :edit, :destroy]
 
     def new
-        @users = User.excluding(@password.users)
         @user_password = UserPassword.new
     end
 
@@ -25,7 +25,7 @@ class SharesController < ApplicationController
     private
 
     def user_password_params
-        params.require(:user_password).permit(:user_id)
+        params.require(:user_password).permit(:user_id, :role)
     end
 
     def set_password
